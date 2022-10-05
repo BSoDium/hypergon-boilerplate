8f927e7cb9577b6ed1655691fd1746dd6dbf5939
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2022-10-05 16:09:09 +0200

    backup difs

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,7 @@
-#define GL_GLEXT_PROTOTYPES 1
-#define GL3_PROTOTYPES 1
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-#include <math.h>
 #include <hypergon.hpp>
 
-// macosx
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#else
-// windows-specific
-#ifdef _WIN32
-#include <windows.h>
-#endif
-// both linux and windows
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GLFW/glfw3.h>
-#endif
-
-int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
+int main()
 {
-  // Initialize the library
-  core::init();
-
   // Start the engine
   core::start();
 
